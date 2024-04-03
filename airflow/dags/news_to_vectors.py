3c65279b2026c8d58c58d093cc26d563bc73cd70
Chidera Mosanya <deramosanya@gmail.com>
2024-04-03 14:04:02 +0100

    added structures for jupyter notebooks

--- airflow/dags/news_to_vectors.py
+++ airflow/dags/news_to_vectors.py
@@ -1,5 +1,15 @@
 # Using the raw News Articles, extract news summary, Named Entities, and
 # sentence-piece vectors to be saved in chromadb
-from airflow import DAG
+import datetime
+import pendulum
+from airflow.decorators import dag, task
 
+
+@dag(dag_id='news-summary',
+     description='Summarize news articles and extract named entities for use in RAG',
+     start_date=pendulum.datetime(2024, 4, 2),
+     catchup=True,
+     )
+def summary_dag_taskflow():
+     pass
 
