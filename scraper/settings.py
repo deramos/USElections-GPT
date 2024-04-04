d3073088e3f63e7b2d375030fcde5b1921dbcfaf
Chidera Mosanya <deramosanya@gmail.com>
2024-04-04 18:31:07 +0100

    make changes to scrapy autothrottle time 30 -> 3

--- scraper/settings.py
+++ scraper/settings.py
@@ -25,7 +25,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 30
+DOWNLOAD_DELAY = 3
 
 # The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 8
@@ -71,7 +71,7 @@ CONCURRENT_REQUESTS_PER_DOMAIN = 8
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 3
 # The maximum download delay to be set in case of high latencies
 AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
