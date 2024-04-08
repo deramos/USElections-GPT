e508430440a78164ad3bb8444997bc39c4529203
Chidera Mosanya <deramosanya@gmail.com>
2024-04-08 16:26:46 +0100

    enabled throttling again

--- scraper/settings.py
+++ scraper/settings.py
@@ -69,14 +69,14 @@ ROBOTSTXT_OBEY = True
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-# AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-# AUTOTHROTTLE_START_DELAY = 3
+AUTOTHROTTLE_START_DELAY = 3
 # The maximum download delay to be set in case of high latencies
-# AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 6
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 8
 # Enable showing throttling stats for every response received:
 # AUTOTHROTTLE_DEBUG = True
 
