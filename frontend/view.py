f7b12588d6e92b1ca783d45b3bd52854beb2a83b
Chidera Mosanya <deramosanya@gmail.com>
2024-04-25 14:47:48 +0100

    added chat websocket to streamlit

--- frontend/view.py
+++ frontend/view.py
@@ -1,6 +1,8 @@
+import uuid
 import streamlit as st
 from websockets import client as ws_client
 
+session_id = uuid.uuid4()
 
 with st.sidebar:
     st.image("docs/img/background_image.tiff", caption='Image Source: AI Generated')
@@ -17,4 +19,16 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    # with ws_client.connect()
+
+    with ws_client.connect(f'ws://localhost:9000/websocket/{session_id}') as websocket:
+        # collect user message and write to chat interface
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+
+        # send the message to llm fastapi websocket
+        websocket.send(prompt)
+        reply = websocket.recv()
+
+        # append reply to the session_state message and write to chat interface
+        st.session_state.messages.append({"role": "assistant", "content": reply})
+        st.chat_message("assistant").write(reply)
