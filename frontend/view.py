e1c9353516dcbbe3417aa2d3492c9670d145df86
Chidera Mosanya <deramosanya@gmail.com>
2024-04-25 17:31:56 +0100

    used asyncio eventloop to create chat interface

--- frontend/view.py
+++ frontend/view.py
@@ -1,34 +1,46 @@
 import uuid
+import asyncio
 import streamlit as st
 from websockets import client as ws_client
 
 session_id = uuid.uuid4()
 
-with st.sidebar:
-    st.image("docs/img/background_image.tiff", caption='Image Source: AI Generated')
-    # st.write('Image Source: AI Generated', caption='')
-    st.write("🇺🇸 RAG enabled Mistral 7B model with knowledge about the US November 2024 electoral process. "
-             "Still a PoC")
 
-st.title("💬 US Election GPT")
+async def websocket_handler():
+    if prompt := st.chat_input():
+        async with ws_client.connect(f'ws://localhost:9000/websocket/{session_id}') as websocket:
+            # collect user message and write to chat interface
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            st.chat_message("user").write(prompt)
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+            # send the message to llm fastapi websocket
+            await websocket.send(prompt)
+            reply = await websocket.recv()
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+            # append reply to the session_state message and write to chat interface
+            st.session_state.messages.append({"role": "assistant", "content": reply})
+            st.chat_message("assistant").write(reply)
 
-if prompt := st.chat_input():
 
-    with ws_client.connect(f'ws://localhost:9000/websocket/{session_id}') as websocket:
-        # collect user message and write to chat interface
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        st.chat_message("user").write(prompt)
+async def main():
+    with st.sidebar:
+        st.image("docs/img/background_image.tiff", caption='Image Source: AI Generated')
+        # st.write('Image Source: AI Generated', caption='')
+        st.write("🇺🇸 RAG based LLM with knowledge about the US November 2024 electoral process. "
+                 "**Still a work-in-progress**")
 
-        # send the message to llm fastapi websocket
-        websocket.send(prompt)
-        reply = websocket.recv()
+    st.title("📨 US Elections GPT")
+    st.write("I am a political analyst with advanced knowledge of the United States electoral process. I will answer "
+             "every question you have about the upcoming November 2024 US General Elections. You can go ahead and ask "
+             "me anything...")
 
-        # append reply to the session_state message and write to chat interface
-        st.session_state.messages.append({"role": "assistant", "content": reply})
-        st.chat_message("assistant").write(reply)
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
+
+    await asyncio.create_task(websocket_handler())
+
+if __name__ == "__main__":
+    asyncio.run(main())
