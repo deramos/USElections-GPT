8592bdb78fdbb623ae474a9bfe3a7f0c27c6294d
Chidera Mosanya <deramosanya@gmail.com>
2024-04-26 18:46:29 +0100

    added airflow dag to run summarization notebook using papermill

--- airflow/dags/news_to_vectors.py
+++ airflow/dags/news_to_vectors.py
@@ -1,15 +1,36 @@
 # Using the raw News Articles, extract news summary, Named Entities, and
 # sentence-piece vectors to be saved in chromadb
 import datetime
+import os
 import pendulum
+from airflow import DAG
+from airflow.models import Variable
 from airflow.decorators import dag, task
+from airflow.operators.python import PythonOperator
+from airflow.operators.python import get_current_context
 
+with DAG(
+        dag_id="news-summary",
+        description='Summarize news articles and extract named entities for use in RAG',
+        start_date=pendulum.datetime(2024, 4, 28),
+        catchup=True,
+        schedule_interval='@daily'
+) as dag:
+    """
+    Summarize news article, extract named entities, and vectorize the text to be saved into chromadb.
+    This is done by running a jupyter notebook `News Summary.ipnb` located in ../notebooks using Papermill
+    https://papermill.readthedocs.io/en/latest/
+    :return:
+    """
 
-@dag(dag_id='news-summary',
-     description='Summarize news articles and extract named entities for use in RAG',
-     start_date=pendulum.datetime(2024, 4, 2),
-     catchup=True,
-     )
-def summary_dag_taskflow():
-     pass
+    context = get_current_context()
+    start_date_str = str(context['data_interval_start'])
+    end_date_str = str(context['data_interval_end'])
 
+    execute_notebook = PapermillOperator(
+        task_id="run_summarize_notebook",
+        input_nb=os.path.join(os.path.dirname(os.path.realpath(__file__)), "notebooks/News Summary.ipynb"),
+        parameters={"batch_date": {'$gte': start_date_str, '$lte': end_date_str}},
+    )
+
+    execute_notebook
