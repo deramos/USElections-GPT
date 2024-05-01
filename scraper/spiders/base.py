f7b8b9c69efe4cd4b00f865f0438d3c797df5f7f
Chidera Mosanya <deramosanya@gmail.com>
2024-05-01 21:30:36 +0100

    removed kafka producer from base spider

--- scraper/spiders/base.py
+++ scraper/spiders/base.py
@@ -27,7 +27,6 @@ class BaseSpider(scrapy.Spider):
     politics_url_pattern = ''
     logger = logging.getLogger(__name__)
     redis_client = RedisService.get_client()
-    producer = Producer({'bootstrap.servers': config.KAFKA_BROKER})
 
     def is_url_visited(self, url):
         """
