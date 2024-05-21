c4cd10a298e6f950859f2c769677160b9002d210
Chidera Mosanya <deramosanya@gmail.com>
2024-05-21 14:57:31 +0100

    done with schedule spider airflow dag

--- airflow/dags/run_scraper.py
+++ airflow/dags/run_scraper.py
@@ -1,10 +1,10 @@
 # Using the raw News Articles, extract news summary, Named Entities, and
 # sentence-piece vectors to be saved in chromadb
-
+import requests
 import pendulum
 from pathlib import Path
+from http import HTTPStatus
 
-import requests
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
@@ -17,54 +17,37 @@ URL = 'http://localhost:9000/scrapers'
 def schedule_scrapers():
     for spider in SCRAPERS:
         # check if spider is running
-        spider_status = requests.post(f'{URL}/{spider}/status')
+        spider_status = requests.get(f'{URL}/{spider}/status').json()
+        # if spider is running, continue
+        if 'status' in spider_status and spider_status['status'] is True:
+            continue
+        # else start it
+        start_spider = requests.get(f'{URL}/{spider}/start')
 
-def execute_notebook():
-    import papermill as pm
-
-    pm.execute_notebook(
-        input_path=Path.joinpath(Path(__file__).parents[2], "notebooks/News Summary.ipynb"),
-        output_path=Path.joinpath(Path(__file__).parents[2], "notebooks/News-Summary-Executed.ipynb"),
-        parameters={"batch_date": {'$gte': start_date_str, '$lte': end_date_str}}
-    )
+        # assert that request was successful
+        assert start_spider.status_code == HTTPStatus.OK
 
 
 with DAG(
-        dag_id="news-summary",
-        description='Summarize news articles and extract named entities for use in RAG',
+        dag_id="run-spider",
+        description='Schedule and start daily spiders',
         start_date=pendulum.datetime(2024, 4, 28),
         catchup=True,
         schedule_interval='@daily'
 ) as dag:
     """
-    Summarize news article, extract named entities, and vectorize the text to be saved into chromadb.
-    This is done by running a jupyter notebook `News Summary.ipnb` located in ../notebooks using Papermill
-    https://papermill.readthedocs.io/en/latest/
+    Schedule the news article scrapy spiders using the FastAPI service created in ../../api/routers/scrapers.
+    Use the /{scraper_name}/start endpoint to start the spider and assert that the response code is HTTP_200_OK
     :return:
     """
 
-    context = get_current_context()
-    start_date_str = str(context['data_interval_start'])
-    end_date_str = str(context['data_interval_end'])
-
     start = EmptyOperator(task_id='start')
 
-    # execute_notebook = PapermillOperator(
-    #     task_id="run_summarize_notebook",
-    #     input_nb=Path.joinpath(Path(__file__).parents[2], "notebooks/News Summary.ipynb"),
-    #     parameters={"batch_date": {'$gte': start_date_str, '$lte': end_date_str}},
-    # )
-
     execute_notebook = PythonOperator(
-        task_id='run_summarize_notebook',
-        python_callable=execute_notebook
-    )
-
-    document_count_qa = PythonOperator(
-        task_id='chromadb_qa',
-        python_callable=check_document_count,
+        task_id='start_spiders',
+        python_callable=schedule_scrapers
     )
 
     end = EmptyOperator(task_id='end')
 
-    start >> execute_notebook >> document_count_qa >> end
+    start >> execute_notebook  >> end
