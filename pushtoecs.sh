8a14bcc307c44d4abecde3f5f379a654ccabf1d5
Chidera Mosanya <deramosanya@gmail.com>
2024-05-29 15:20:27 +0100

    made local changes to push to ecr

--- pushtoecs.sh
+++ pushtoecs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Build the images
+docker compose build
+
 # Define the ECR repository URL
 ECR_REPOSITORY_URL="697335112974.dkr.ecr.us-east-1.amazonaws.com"
+ECR_REPOSITORY_NAME="us-election-gpt"
 
 # Login to ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REPOSITORY_URL
@@ -11,7 +15,7 @@ images=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep "uselections")
 
 # Check if any images were found
 if [ -z "$images" ]; then
-  echo "No images found with 'uselections' in the name."
+  echo "No 'us-elections' images found."
   exit 1
 fi
 
@@ -22,13 +26,17 @@ for image in $images; do
   image_tag=$(echo $image | cut -d ':' -f 2)
 
   # Construct the new ECR tag
-  ecr_tag="${ECR_REPOSITORY_URL}/${image_name}:${image_tag}"
+  ecr_tag="${ECR_REPOSITORY_URL}/${ECR_REPOSITORY_NAME}:${image_name}"
 
   # Tag the image
   echo "Tagging image $image as $ecr_tag"
   docker tag $image $ecr_tag
 
-  # Optionally, push the image to ECR
-  # echo "Pushing image $ecr_tag to ECR"
-  docker push $ecr_tag
+  # Push the image to ECR
+  echo "Pushing image $ecr_tag to ECR"
+  # docker push $ecr_tag
+  docker push ${ECR_REPOSITORY_URL}/${ECR_REPOSITORY_NAME}:${image_name}
+
+  # Untag to preserve original image names
+  docker rmi $ecr_tag
 done
