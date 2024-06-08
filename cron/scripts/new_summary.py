408642f3748fd673206b78249063f1238b712cde
Chidera Mosanya <deramosanya@gmail.com>
2024-06-08 11:55:17 +0100

    added news summary cron script

--- cron/scripts/new_summary.py
+++ cron/scripts/new_summary.py
@@ -1,8 +1,15 @@
+from dbservices.redisservice import RedisService
 from datetime import datetime, timedelta
+
+redis_client = RedisService.get_client()
+
+redis_key = 'scraped-news-count'
 
 
 def execute_notebook():
     import papermill as pm
+
+    news_count = int(redis_client.get(redis_key)) if redis_client.get(redis_key) is not None else 0
 
     end_date, start_date = datetime.now(), datetime.now() - timedelta(days=1, minutes=2)
     end_date_str = end_date.strftime("%Y-%m-%d %H:%M:%S")
