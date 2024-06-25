c1a4e2780de2307692e835cc25176d323a9b406a
Chidera Mosanya <deramosanya@gmail.com>
2024-06-25 11:29:01 +0100

    added chat post endpoint

--- api/routers/chats.py
+++ api/routers/chats.py
@@ -1,6 +1,10 @@
+import asyncio
 import logging
+
 from util.chat_util import LLMUtil
-from fastapi import APIRouter, WebSocket
+from fastapi import HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi import APIRouter, WebSocket, Path, Request
 from langchain_core.runnables.history import RunnableWithMessageHistory
 
 logging.basicConfig(level=logging.INFO)
@@ -38,3 +42,26 @@ async def chat(websocket: WebSocket, session_id: str):
     except Exception as e:
         logger.info(f"WebSocket connection closed: {e}")
 
+
+@router.post('/{session_id}')
+async def http_chat(
+        request: Request,
+        session_id: str = Path(..., description='The session ID')
+):
+    data = await request.json()
+    message = data.get("message")
+
+    if not message:
+        return HTTPException(status_code=400, detail='No input message received.')
+
+    chat_response = rag_llm.invoke(
+        {"input": message},
+        config={"configurable": {"session_id": session_id}})
+
+    async def stream_response():
+        for part in chat_response.split(r"\w+|\W+"):
+            yield f"data: {part}\n\n"
+            await asyncio.sleep(0.1)
+        yield "data: [END]\n\n"
+
+    return StreamingResponse(stream_response(), media_type="text/event-stream")
